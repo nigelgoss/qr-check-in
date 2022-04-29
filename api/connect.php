263d8a0069f314ae24dc4008608338af767f6158
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2022-04-29 17:27:53 +0100

    Update connect.php

--- api/connect.php
+++ api/connect.php
@@ -4,7 +4,7 @@ require_once('env.php');
 
 try {
 	
-	$conn = new PDO('sqlsrv:server='.DB_HOST.'; Database='DB_DATABASE, DB_USERNAME, DB_PASSWORD);  
+	$conn = new PDO('sqlsrv:server='.DB_HOST.'; Database='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);  
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
 	
 } catch(Exception $e) {
